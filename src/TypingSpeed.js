07585cc9075062d0ef19e52da42d8532dfa91b36
minhajul-karim <minhajul.kaarim@gmail.com>
2021-04-08 11:00:11 +0600

    Add start button and timer

--- src/TypingSpeed.js
+++ src/TypingSpeed.js
@@ -5,8 +5,10 @@ export default function TypingSpeed() {
     <div className="typing_container">
       <h1 className="typing_container__title">Typing Speed Test</h1> 
       <p className="typing_container__test">Long time ago, there lived four cows in a jungle. They were best friends. They always grazed together and saved each other in danger. Due to this unity, even the tigers and lions could not kill and eat them. A lion had indeed an eye upon these cows. But he could never find the right opportunity to make them his prey. One day, the cows had a fight among themselves. Each one grazed separately her own way. The lion and the tiger got to know about the fight of the cows. So, they planned to trick all the cows. One day, lion and the tiger found the right opportunity to attack them.</p>
-      <p className="typing_container__speed">Speed <strong>50</strong> WPM</p>
-      <p className="typing_container__accuracy">Accuracy <strong>90%</strong></p>
+      <p className="typing_container__speed">Speed <strong>0</strong> WPM</p>
+      <p className="typing_container__accuracy">Accuracy <strong>0%</strong></p>
+      <button className="typing_container__btn">start</button>
+      <p className="typing_container__time">0s</p>
       <textarea className="typing_container__textarea" />
     </div>
   )
